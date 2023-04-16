de89aa41f435c1ca31d5083c1e8bc97cce6e30fe
julabrego <abrego.jul@gmail.com>
2023-04-16 19:28:40 -0300

    correcciones

--- src/components/EmptySlot/EmptySlot.jsx
+++ src/components/EmptySlot/EmptySlot.jsx
@@ -2,35 +2,34 @@ import { useState } from "react";
 import Slot from "./Slot";
 import ItemDropOff from "../ItemDropOff/ItemDropOff";
 
-function EmptySlot({ id, recievedId, item,gridHandlerStyle }) {
+function EmptySlot({ id, recievedId, item, gridHandlerStyle, hidden = false }) {
     //id es el id del emptyslot que va a venir del MAP, ahora está hardcodeado
     //item viene lo mas bien, hay que recordar hace el onDrop con el flag y como eliminé el bug
     //boardLenght el tipo deberia matchear el board con las truePositions.map
 
     const [itemSaved, setItemSaved] = useState(null); // esta almacena recievedId en un estado, y lo inicializa en NULL
     const [flag, setFlag] = useState(false); // la flag determina si puede recibir o no el elemento
-    
 
-    const clearSlot = ()=>{
+    const clearSlot = () => {
         //es el evento del OnClick del boton que limpia el slot
-        if(flag){
+        if (flag) {
             setItemSaved(null)
             setFlag(false)
             setItemDropLength(null)
-            gridHandlerStyle((id),itemSaved.slots,true)
         }
     }
 
     //console.log(itemSaved);
 
     const [itemDropLength, setItemDropLength] = useState(null);
+
     return (
-        <div style={itemDropLength}
+        <div style={itemDropLength} className={hidden && 'hidden'}
             onDragOver={(e) => {
                 e.preventDefault();
             }}
             onDrop={() => {
-                if(!flag && recievedId!==null){
+                if (!flag && recievedId !== null) {
                     setItemSaved(item)
                     setFlag(true)
                     // setItemDropLength({
@@ -39,13 +38,13 @@ function EmptySlot({ id, recievedId, item,gridHandlerStyle }) {
                     //     gridRowStart: 1,
                     //     gridRowEnd:2
                     // })
-                    if(item.slots>1){
-                        gridHandlerStyle((id),item.slots,false)
+                    if (item.slots > 1) {
+                        gridHandlerStyle((id), item.slots, false)
                     }
                 };
             }}
         >
-            {itemSaved === null ? <Slot id={id}/> : <ItemDropOff id={id} clearSlot={clearSlot} item={itemSaved}/>}
+            {itemSaved === null ? <Slot id={id} /> : <ItemDropOff id={id} clearSlot={clearSlot} item={itemSaved} />}
         </div>
     );
 }
